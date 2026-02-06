effe223e969bd448496c0fae5495104386790182
Tune02 RoboTech <huseyinsozen88@windowslive.com>
2026-02-06 12:53:54 +0300

    API Update

--- api/logout.php
+++ api/logout.php
@@ -1,3 +1,8 @@
 <?php
+// File: /public/logout.php
 declare(strict_types=1);
-require __DIR__ . '/../public/logout.php';
+
+require __DIR__ . "/../app/bootstrap.php";
+
+session_destroy();
+redirect("/login.php");
